12da31dbcc5730c0ddf879160955c91c6ececfd0
nabiursiddique <nabiursiddique01@gmail.com>
2023-01-06 18:48:55 +0600

    Navbar is added

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import Blogs from "../../Pages/Blogs/Blogs";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/blogs',
+                element: <Blogs></Blogs>
+            }
+        ]
+    }
+]);
+export default router;
