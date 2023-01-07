b6adbd4dadcd800e4e0430affc3011a91be57b20
nabiursiddique <nabiursiddique01@gmail.com>
2023-01-07 21:11:06 +0600

    Sign up form is completed

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,18 +1,25 @@
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import { FaGoogle } from 'react-icons/fa';
 
-const Login = () => {
+const Signup = () => {
     const { register, handleSubmit, formState: { errors }
     } = useForm();
-    const handleLogin = data => {
+    const handleSignup = data => {
         console.log(data)
     };
     return (
         <div className='my-20 flex justify-center items-center'>
             <div className='glass p-10 w-96'>
-                <h2 className='text-4xl font-bold text-center mb-5'>LOG IN</h2>
-                <form onSubmit={handleSubmit(handleLogin)}>
+                <h2 className='text-4xl font-bold text-center mb-5'>SIGN UP</h2>
+                <form onSubmit={handleSubmit(handleSignup)}>
+                    <div className="form-control w-full max-w-sm">
+                        <label className="label"><span className="label-text font-bold text-white">Enter Your Name</span>
+                        </label>
+                        <input {...register("name", { required: 'Name is required' })} type="text" className="input input-bordered w-full max-w-xs" />
+                        {errors.name && <p role='alert' className='text-blue-400'>{errors.name?.message}</p>}
+                    </div>
                     <div className="form-control w-full max-w-sm">
                         <label className="label"><span className="label-text font-bold text-white">Enter Your Email</span>
                         </label>
@@ -22,16 +29,20 @@ const Login = () => {
                     <div className="form-control w-full max-w-sm">
                         <label className="label"><span className="label-text font-bold text-white">Enter Your Password?</span>
                         </label>
-                        <input type="password" {...register("password", { required: 'Password is required', minLength: { value: 6, message: 'Password must be at least 6 characters' } })} className="input input-bordered w-full max-w-xs" />
+                        <input type="password" {...register("password", {
+                             required: 'Password is required', 
+                             minLength: { value: 6, message: 'Password must be at least 6 characters' },
+                             pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: 'Password must be strong' }
+                              })} className="input input-bordered w-full max-w-xs" />
                         {errors.password && <p role='alert' className='text-blue-400'>{errors.password?.message}</p>}
                         <label className="label">
                             <span className="label-text-alt text-blue-400">Forget password?</span>
                         </label>
                     </div>
-                    <button className="btn w-full hover:bg-gradient-to-r from-gray-400 to-gray-600 glass text-white mt-5" type='submit'>LOG IN</button>
+                    <button className="btn w-full hover:bg-gradient-to-r from-gray-400 to-gray-600 glass text-white mt-5" type='submit'>SIGN UP</button>
 
                     {/* New to doctors portal section */}
-                    <p className='text-center mt-3'>New to used phone shop? <Link className='text-blue-400' to='/signup'>Sign up</Link></p>
+                    <p className='text-center mt-3'>Already have an account? <Link className='text-blue-400' to='/login'>Log In</Link></p>
                     <div className="divider">OR</div>
                     <button className="btn btn-outline w-full font-semibold"><span className='text-xl mr-2'><FaGoogle /></span> CONTINUE WITH GOOGLE</button>
 
@@ -41,4 +52,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Signup;
