adca472c4ed0424c4c037ce68548c3f541af6743
nabiursiddique <nabiursiddique01@gmail.com>
2023-01-07 17:14:48 +0600

    Login form validation with react hook is completed

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { FaGoogle } from 'react-icons/fa';
 
 const Login = () => {
+    const { register, handleSubmit, formState: { errors }
+    } = useForm();
+    const handleLogin = data => {
+        console.log(data)
+    };
     return (
-        <div>
-            <h2>This is login</h2>
+        <div className='my-20 flex justify-center items-center'>
+            <div className='glass p-10 w-96'>
+                <h2 className='text-4xl font-bold text-center mb-5'>LOG IN</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+                    <div className="form-control w-full max-w-sm">
+                        <label className="label"><span className="label-text font-bold text-white">Enter Your Email</span>
+                        </label>
+                        <input {...register("email", { required: 'Email address is required' })} type="email" className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p role='alert' className='text-blue-400'>{errors.email?.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-sm">
+                        <label className="label"><span className="label-text font-bold text-white">Enter Your Password?</span>
+                        </label>
+                        <input type="password" {...register("password", { required: 'Password is required', minLength: { value: 6, message: 'Password must be at least 6 characters' } })} className="input input-bordered w-full max-w-xs" />
+                        {errors.password && <p role='alert' className='text-blue-400'>{errors.password?.message}</p>}
+                        <label className="label">
+                            <span className="label-text-alt text-blue-400">Forget password?</span>
+                        </label>
+                    </div>
+                    <button className="btn w-full hover:bg-gradient-to-r from-gray-400 to-gray-600 glass text-white mt-5" type='submit'>LOG IN</button>
+
+                    {/* New to doctors portal section */}
+                    <p className='text-center mt-3'>New to used phone shop? <Link className='text-blue-400' to='/'>Sign up</Link></p>
+                    <div className="divider">OR</div>
+                    <button className="btn btn-outline w-full font-semibold"><span className='text-xl mr-2'><FaGoogle /></span> CONTINUE WITH GOOGLE</button>
+
+                </form>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -10,20 +10,20 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 ">
-                        <li><Link to='/'>Home</Link></li>
-                        <li tabIndex={0}>
-                            <Link to='/' className="justify-between">
+                        <li className='font-bold uppercase'><Link to='/'>Home</Link></li>
+                        <li className='uppercase font-bold' tabIndex={0}>
+                            <Link to='/' className="justify-between uppercase font-bold">
                                 Category
                                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
                             </Link>
                             <ul className="p-2 font-bold">
-                                <li><Link to='/'>Samsung</Link></li>
-                                <li><Link to='/'>Iphone</Link></li>
-                                <li><Link to='/'>Google Pixel</Link></li>
+                                <li className='font-bold uppercase'><Link to='/'>Samsung</Link></li>
+                                <li className='font-bold uppercase'><Link to='/'>Iphone</Link></li>
+                                <li className='font-bold uppercase'><Link to='/'>Google Pixel</Link></li>
                             </ul>
                         </li>
-                        <li><Link to='/blogs'>Blogs</Link></li>
-                        <li><Link to='/login'>Log In</Link></li>
+                        <li className='font-bold uppercase'><Link to='/blogs'>Blogs</Link></li>
+                        <li className='font-bold uppercase btn-secondary rounded'><Link to='/login'>Log In</Link></li>
                     </ul>
                 </div>
                 <Link to='/' className="btn btn-ghost normal-case text-xl font-bold ">USED PHONE SHOP</Link>
@@ -32,20 +32,20 @@ const Navbar = () => {
             {/* For big screen devices */}
             <div className="navbar-end hidden lg:flex ">
                 <ul className="menu menu-horizontal px-1 z-10">
-                    <li><Link to='/'>Home</Link></li>
-                    <li tabIndex={0}>
+                    <li className='font-bold uppercase'><Link to='/'>Home</Link></li>
+                    <li className='uppercase font-bold' tabIndex={0}>
                         <Link to='/'>
                             Category
                             <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
                         </Link>
                         <ul className="p-2 font-bold">
-                            <li><Link to='/'>Samsung</Link></li>
-                            <li><Link to='/'>Iphone</Link></li>
-                            <li><Link to='/'>Google Pixel</Link></li>
+                            <li className='font-bold uppercase'><Link to='/'>Samsung</Link></li>
+                            <li className='font-bold uppercase'><Link to='/'>Iphone</Link></li>
+                            <li className='font-bold uppercase'><Link to='/'>Google Pixel</Link></li>
                         </ul>
                     </li>
-                    <li><Link to='/blogs'>Blogs</Link></li>
-                    <li><Link to='/login'>Log In</Link></li>
+                    <li className='font-bold uppercase'><Link to='/blogs'>Blogs</Link></li>
+                    <li className='font-bold uppercase btn-secondary rounded'><Link to='/login'>Log In</Link></li>
                 </ul>
             </div>
         </div>
